cc969db1d2b3aaa4ab5b16247035dd8a4af6ead9
yanDavid21 <yanDavid21@users.noreply.github.com>
2020-07-25 20:02:12 -0400

    Removed unneeded new line characters
    controllerImpl added unnecessary new line characters in input

--- src/client/controller/MultiChatControllerImpl.java
+++ src/client/controller/MultiChatControllerImpl.java
@@ -18,38 +18,41 @@ public class MultiChatControllerImpl implements MultiChatController, Features {
 
   @Override
   public void run() {
-    while (model.isConnectionRunning()) {
+    while(model.isConnectionRunning()) {
       String line = model.getSocketInput();
+
       if (line.startsWith("SUBMITNAME")) {
-        String username = view.getName("Please select a screen name:");
+        String username = view.getName("Choose a screen name:");
         model.sendText(username);
       } else if (line.startsWith("SUBMITANOTHERNAME")) {
-        String username = view.getName("Please select another screen name:");
+        String username = view.getName("Please select a different screen name:");
         model.sendText(username);
       } else if (line.startsWith("NAMEACCEPTED")) {
         view.setTextFieldEditable(true);
         view.display();
       } else if (line.startsWith("MESSAGE ")) {
-        view.appendChatLog(line.substring(8) + "\n", "black", true);
+        view.appendChatLog(line.substring(8), "black", true);
       } else if (line.startsWith("MESSAGEUSERJOINED ")) {
-        view.appendChatLog(line.substring(18) + "\n", "green", true);
+        view.appendChatLog(line.substring(18), "green", true);
       } else if (line.startsWith("MESSAGEUSERLEFT ")) {
-        view.appendChatLog(line.substring(16) + "\n", "red", true);
+        view.appendChatLog(line.substring(16), "red", true);
       } else if (line.startsWith("MESSAGEWELCOME ")) {
-        view.appendChatLog(line.substring(15) + "\n", "blue", false);
+        view.appendChatLog(line.substring(15), "blue", false);
       } else if (line.startsWith("ACTIVEUSERLIST ")) {
-        List<String> activeUserList = Arrays.asList(line.substring(15).split(","));
-        view.setActiveUsers(activeUserList);
+        String[] arr = line.substring(15).split(",");
+        List<String> names = Arrays.asList(arr);
+        view.setActiveUsers(names);
       } else if (line.startsWith("MESSAGEHELP ")) {
-        view.appendChatLog(line.substring(12) + "\n", "orange", false);
+        view.appendChatLog(line.substring(12), "orange", false);
       } else if (line.startsWith("ACTIVESERVERLIST ")) {
-        List<String> activeUserList = Arrays.asList(line.substring(17).split(","));
-        view.setActiveServers(activeUserList);
+        String[] arr = line.substring(17).split(",");
+        List<String> servers = Arrays.asList(arr);
+        view.setActiveServers(servers);
       } else if (line.startsWith("REQUESTEDNEWROOM ")) {
         try {
           MultiChatModel newModel = model.switchPorts(line.substring(17));
           model.sendText("/quit");
-          view.appendChatLog("Successfully left.\n", "red", false);
+          view.appendChatLog("Successfully left.", "red", false);
           model = newModel;
         } catch (IOException e) {
           model.sendText("UNSUCCESSFULROOMCHANGE Cannot connect to new chat room.");
